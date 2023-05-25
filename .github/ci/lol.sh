65bcb4af6c6321c626bebb2696cefb9b034ff5da
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-05-26 00:53:01 +0200

    wtf

--- .github/ci/lol.sh
+++ .github/ci/lol.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-## on push branch~=ma..
+## on push branch=main
 ## on push branch~=gh-readonly-queue/main/.*
 
 set -euxo pipefail
 
 echo Hello World!
 echo Hello World!
-dsf
-asdfasdf
