4c57ef98a83d5524c831106fd86dc85c3cd96d8e
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-05-29 17:11:52 +0200

    lol

--- .github/ci/lol.sh
+++ .github/ci/lol.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ## on push branch=main
+## on push branch=wtflol
 ## on push branch~=gh-readonly-queue/main/.*
 ## on pull_request
 
@@ -13,4 +14,4 @@ if [ -f /ci/cache/wtf.txt ]; then
     cat /ci/cache/wtf.txt
 fi
 
-echo wtf > /ci/cache/wtf.txt
+echo wtf > /ci/cache/wtflol.txt
