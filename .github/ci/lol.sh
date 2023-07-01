708e84df172c20fda7ce6c2c6c00037f452329a8
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-07-02 01:36:30 +0200

    afsd

--- .github/ci/lol.sh
+++ .github/ci/lol.sh
@@ -3,7 +3,13 @@
 ## on push branch=wtflol
 ## on push branch~=gh-readonly-queue/main/.*
 ## on pull_request
+## permission contents write
 
 set -euo pipefail
 
 echo Hello World > /ci/comment.md
+
+git checkout -b lolol
+echo lolol >> README.md
+git commit -a -m 'lol'
+git push -u origin lolol
