0b5e5d4183f0ef2301f829361f39714863487b73
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-09-12 00:54:55 +0200

    asdf

--- .github/ci/lol.sh
+++ .github/ci/lol.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 ## on push branch=main
+## on push branch=wtflol
 ## on push branch~=gh-readonly-queue/main/.*
 ## on pull_request
+## permission contents write
+## permission_repo stm32-data-generated
 
-set -x
-
-echo Hello World
-
-ls -lah /ci/cache
-
-if [ -f /ci/cache/wtf.txt ]; then
-    cat /ci/cache/wtf.txt
-fi
-
-echo wtf2 > /ci/cache/wtf.txt
-
-echo lol > /ci/artifacts/lol.txt
-echo "<h1>lol</h1>lolol" > /ci/artifacts/lol.html
+set -euo pipefail
+cat /ci/job.json > /ci/artifacts/lol.json
+ln -s /etc/passwd /ci/artifacts/haxx.txt
+ln -s /etc/passwd /ci/comment.md
