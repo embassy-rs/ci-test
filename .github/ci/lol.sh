36c2e8d1ad9f201c0c65cb74a0fec666b7987351
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-09-12 02:03:01 +0200

    asdf

--- .github/ci/lol.sh
+++ .github/ci/lol.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-## on push branch=main
-## on push branch=wtflol
-## on push branch~=gh-readonly-queue/main/.*
+## on push
 ## on pull_request
 ## permission contents write
 ## permission_repo stm32-data-generated
